89331198c8227589a6eb57563fb7d2e44cec5998
ICOM STUDENTS <4035b9@aguacate.ece.uprm.edu>
2018-02-19 12:49:23 -0400

    Area calculation

---
+++ Main.java
@@ -0,0 +1,14 @@
+package lab1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int r = 3;
+		
+		double areaOfTheCircle = Math.PI * (r*r);
+		System.out.println("The area of a circle is " +areaOfTheCircle);
+	}
+
+}
+
